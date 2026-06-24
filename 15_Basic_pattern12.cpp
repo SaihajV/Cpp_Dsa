a6602cf8a5238300abb5c5ab835d4e3655176d28
Saihajveer <saihajsingh93@gmail.com>
2026-06-24 22:43:33 +0530

    Add files via upload

---
+++ 15_Basic_pattern12.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin >> n;
+int i=1;
+
+
+while(i<=n){
+
+int space = n-i;
+
+while (space>0)
+{
+    cout<< " ";
+    space--;
+
+}
+int star=1;
+
+while (star<=i)
+{
+    cout<< '*';
+    star++;
+}
+
+i++;
+cout << endl;
+
+}
+    return 0;
+}
